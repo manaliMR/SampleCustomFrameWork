b0fa0e141dbbbe1f07457a28e9e8b870c597573d
Manali Rami <73849042+Manali92@users.noreply.github.com>
2020-12-28 17:34:49 -0500

    Sample Framework added

---
+++ SampleCustomFrameWork/SampleCustomFrameWork/SampleCustomFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  SampleCustomFrameWork.h
+//  SampleCustomFrameWork
+//
+//  Created by Manali Rami on 2020-12-28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleCustomFrameWork.
+FOUNDATION_EXPORT double SampleCustomFrameWorkVersionNumber;
+
+//! Project version string for SampleCustomFrameWork.
+FOUNDATION_EXPORT const unsigned char SampleCustomFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleCustomFrameWork/PublicHeader.h>
+
+
---
+++ SampleCustomFrameWork/SampleCustomFrameWork/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  SampleCustomFrameWork
+//
+//  Created by Manali Rami on 2020-12-28.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func sampleExecution() -> String {
+        return "Hello, You did great job🥂, You have created your first custom Framework Successfully🤑"
+    }
+}
